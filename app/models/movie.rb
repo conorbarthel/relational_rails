e8c5e04c56a674702e98dee78603ece0b83a20f1
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-02-03 13:29:23 -0700

    Feat: add validation model tests to Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  validates_presence_of :title,
+                        :length,
+                        :genre,
+                        :released
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  it {should belong_to :director}
+  describe 'relationships' do
+    it {should belong_to :director}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :length}
+    it {should validate_presence_of :genre}
+    it {should validate_presence_of :released}
+  end
 end
