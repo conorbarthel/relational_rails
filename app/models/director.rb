a11ad92cff92ad5e1ff487436f9316d429be8e41
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-02-03 13:19:51 -0700

    Feat: add validation model tests to Director

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,3 +1,7 @@
 class Director < ApplicationRecord
   has_many :movies
+
+  validates_presence_of :name,
+                        :active,
+                        :awards_won
 end
--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Director do
-  it {should have_many :movies}
+  describe 'relationships' do
+    it {should have_many :movies}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :active}
+    it {should validate_presence_of :awards_won}
+  end
 end
