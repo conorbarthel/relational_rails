13c0edda0d5e6e0b2314fb676f31e2812c5e92df
Conor Barthel <conor.barthel@gmail.com>
2022-02-03 08:37:18 -0700

    Fix: One last add before push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/disc_manufaturer', to:''
+  get '/producers', to:'producers#index'
+  get '/producers/:id', to:'producers#show'
 end
