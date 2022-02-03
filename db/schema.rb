4347a48b99281c4391bcada7b4c66e85a7d31d2f
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-02-02 18:03:29 -0700

    Feat: Fix gitignore file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_225157) do
+ActiveRecord::Schema.define(version: 2022_02_02_225404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2022_02_02_225157) do
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "director_id"
+    t.index ["director_id"], name: "index_movies_on_director_id"
   end
 
+  add_foreign_key "movies", "directors"
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
-    with.test_framework :RSpec
+    with.test_framework :rspec
     with.library :rails
   end
-end 
+end
