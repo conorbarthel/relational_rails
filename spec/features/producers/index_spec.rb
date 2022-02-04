830e0ef2f808f50b0880f0ccadc872e72f55769c
Conor Barthel <conor.barthel@gmail.com>
2022-02-04 16:30:40 -0700

    Feat: Pass user story 7

--- spec/features/producers/index_spec.rb
+++ spec/features/producers/index_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe 'the producer index page' do
     expect(page).to have_content(innova.name)
     expect(page).to have_content(discraft.name)
   end
+
+  it "displays the name of producers in order from most recently created" do
+    innova = Producer.create!(name:'Innova', number_of_athletes: 56, free_shipping: true)
+    discraft = Producer.create!(name:'Discraft', number_of_athletes: 65, free_shipping: true)
+
+    visit "/producers"
+    save_and_open_page
+    expect(discraft.name).to appear_before(innova.name)
+  end
 end
