c3f736e43f8a2eca82573181e9976166232d8a9b
Conor Barthel <conor.barthel@gmail.com>
2022-02-06 14:05:52 -0700

    Test: new disc page feature test

---
+++ spec/features/discs/new_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'index' do
+  before(:each) do
+    innova = Producer.create!(name:'Innova', number_of_athletes: 56, free_shipping: true)
+    discraft = Producer.create!(name:'Discraft', number_of_athletes: 65, free_shipping: true)
+    zone = discraft.discs.create!(name: 'Zone', plastic: 'Glow', speed: 4, available: true)
+    leopard = innova.discs.create!(name: 'Leopard', plastic: 'DX', speed: 6, available: true)
+  end
+
+  it "creates a new discs, redirects to the index page, and shows the discs there" do
+    visit "/discss/new"
+    fill_in("Name", with:"Undertaker")
+    fill_in("Plastic", with:"ESP")
+    fill_in("Speed", with:9)
+    check('Available')
+    producer('discraft')
+    click_button("Create Disc")
+    #save_and_open_page
+
+    expect(current_path).to eq('/discss')
+    expect(page).to have_content("Undertaker")
+  end
+end
