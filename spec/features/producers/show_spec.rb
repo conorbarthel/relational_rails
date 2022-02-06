5350e60e19aeecd4f0614efcc156c283a88c723b
Conor Barthel <conor.barthel@gmail.com>
2022-02-06 08:48:59 -0700

    Feat: User story 10 passes

--- spec/features/producers/show_spec.rb
+++ spec/features/producers/show_spec.rb
@@ -1,23 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe 'the producer index page' do
+  before(:each) do
+    @innova = Producer.create(name:'Innova', number_of_athletes: 56, free_shipping: true)
+    @discraft = Producer.create(name:'Discraft', number_of_athletes: 65, free_shipping: true)
+  end
+
   it 'displays the name of all producers' do
-    innova = Producer.create(name:'Innova', number_of_athletes: 56, free_shipping: true)
-    discraft = Producer.create(name:'Discraft', number_of_athletes: 65, free_shipping: true)
 
-    visit "/producers/#{innova.id}"
+    visit "/producers/#{@innova.id}"
 
-    expect(page).to have_content(innova.name)
-    expect(page).to have_content(innova.number_of_athletes)
-    expect(page).to have_content(innova.free_shipping)
-    expect(page).to_not have_content(discraft.name)
+    expect(page).to have_content(@innova.number_of_athletes)
+    expect(page).to have_content(@innova.free_shipping)
+    expect(page).to_not have_content(@discraft.name)
   end
 
   it "shows a count of number of discs" do
-    innova = Producer.create(name:'Innova', number_of_athletes: 56, free_shipping: true)
 
-    visit "/producers/#{innova.id}"
+    visit "/producers/#{@innova.id}"
 
-    expect(page).to have_content(innova.count)
+    expect(page).to have_content(@innova.count)
+  end
+
+  it 'has a link to its discs' do
+
+    visit "/producers/#{@innova.id}"
+    save_and_open_page
+    click_on "#{@innova.name} discs"
+    expect(current_path).to eq("/producers/#{@innova.id}/discs")
   end
 end
